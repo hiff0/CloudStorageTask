dc7099e250d7392375b984cd0f21228b2ebf629c
Vlad Petrov <hiffchanel@gmail.com>
2022-12-28 00:01:28 +0300

    push project

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
 require('dotenv').config();
+import mongoose from 'mongoose';
 
 import api from './api';
 import { getConfig } from './config';
 
 const PORT = getConfig().PORT || 8080;
+const dbConnet = getConfig().TEST_DATABASE_CONNECTION || "mongodb://127.0.0.1:27023/admin?replicaSet=dbrs&directConnection=true";
 
-api.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
+//FIXED добавить обработку ошибки, если TEST_DATABASE_CONNECTION undefined и вынести все в файл database
+const start = async () => {
+    try {
+        await mongoose.connect(dbConnet)
+            .then(() => {
+                console.log('DB connect')
+            })
+        api.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}`);
+        });
+    } catch (error) {
+
+    }
+}
+
+start();
